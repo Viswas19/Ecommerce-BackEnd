dc46a764a223e774d4797e303e83fd5f6a236e84
Ajayviswas <ajayviswas19august@gmail.com>
2025-04-09 15:06:48 +0530

    Removed the Unwanted Folders

--- src/main/java/com/example/project/service/interf/AddressService.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.service.interf;
-
-import com.example.project.dto.AddressDto;
-import com.example.project.dto.Response;
-
-public interface AddressService {
-    Response saveandUpdateAddress(AddressDto addressDto);
-}
